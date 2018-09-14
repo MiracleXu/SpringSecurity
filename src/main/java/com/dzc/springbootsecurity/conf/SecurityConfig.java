d796724ea9861264e4b50b14705251faf66daf12
dzc <dongzzzc@gmail.com>
2018-09-14 17:11:58 +0800

    add

---
+++ src/main/java/com/dzc/springbootsecurity/conf/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.dzc.springbootsecurity.conf;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // 定制请求授权规则
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/level1/**").hasRole("VIP1")
+                .antMatchers("/level2/**").hasRole("VIP2")
+                .antMatchers("/level3/**").hasRole("VIP3");
+
+        // 开启自动配置的登录功能
+        http.formLogin();
+
+
+        http.logout().logoutSuccessUrl("/");
+    }
+}
